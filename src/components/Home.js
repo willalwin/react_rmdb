3ae906896ae32cad77f17c21d85d553bce83a43c
willalwin <willalwin@msn.com>
2021-10-26 17:16:40 -0700

    Finished home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,47 +1,61 @@
 import React, {useState, useEffect} from 'react';
 //api
-import API from '../API';
+//import API from '../API';
 
-import {POSTER_SIZR, BACKDROP_SIZE, IMAGE_BASE_URL} from '../config';
+import {POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL} from '../config';
 //components
+import HeroImage from './HeroImage';
+import Grid from './Grid';
+import Thumb from './Thumb';
+import Spinner from './Spinner';
+import Button from './Button';
 
 //hook
+import {useHomeFetch} from '../hooks/useHomeFetch';
 
 //image
 import NoImage from '../images/no_image.jpg';
+import SearchBar from './SearchBar';
 
 const Home = () => {
-    const [state, setState] = useState();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
+    const {
+        state, 
+        loading, 
+        error, 
+        searchTerm, 
+        setSearchTerm,
+        setIsLoadingMore
+    } = useHomeFetch();
 
-    const fetchMovies = async (page, searchTerm="") => {
-        try{
-            setError(false);
-            setLoading(true);
-
-            const movies = await API.fetchMovies(searchTerm, page);
-            console.log(movies);
-
-            setState(prev => ({
-                ...movies, 
-                results: 
-                    page > 1 ? [...prev.results, ...movies.results]:[...movies.results]
-            }));
-        } catch(error){
-            setError(true);
-        }
-        setLoading(false); 
-    };
-
-    //initial render
-    useEffect(() => {
-        fetchMovies(1)
-    }, []);
-
-    console.log(state);
-
-    return <div>Home Page</div>;
+    if(error) return <div>Something went wrong...</div>
+    return (
+        <>
+            {!searchTerm && state.results[0] ? 
+            <HeroImage 
+                image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+                title={state.results[0].original_title}
+                text={state.results[0].overview}
+            /> 
+            : null
+            } 
+            <SearchBar setSearchTerm={setSearchTerm}/>
+            <Grid header={searchTerm ? 'Search Results':'Popular Movies'}>
+              {state.results.map(movie => (
+                  <Thumb 
+                    key={movie.id}
+                    clickable
+                    image={movie.poster_path ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path : NoImage}
+                    movieId={movie.id}
+                    />
+              ))}  
+            </Grid>  
+            {loading && <Spinner/> } 
+            {state.page<state.total_pages && !loading && (
+                <Button text='Load More' callback={() => setIsLoadingMore(true)}/>
+            )}      
+        </>    
+    
+    );
 };
 
 export  default Home;
